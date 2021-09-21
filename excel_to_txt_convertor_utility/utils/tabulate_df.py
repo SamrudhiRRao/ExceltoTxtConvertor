9c03fd4409481e3ccdd1eb7ef295b8b13cf7647f
SamrudhiRRao <raosamrudhi@gmail.com>
2021-09-21 10:13:20 +0530

    Task1- Added all files

---
+++ excel_to_txt_convertor_utility/utils/tabulate_df.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from tabulate import tabulate
+
+def tabulate_df(df,csv_file):
+    txtcode = tabulate(df, headers='keys', tablefmt='psql')
+    txtfile = csv_file.split(".csv")[0] + ".txt"
+    return txtcode,txtfile
